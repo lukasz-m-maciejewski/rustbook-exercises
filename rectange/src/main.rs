399985286daf7168d6ff37883a356b1490440aea
Lukasz Maciejewski <lukasz.m.maciejewski@pm.me>
2020-02-10 22:48:23 +0100

    adding area-as-method impl

--- rectange/src/main.rs
+++ rectange/src/main.rs
@@ -4,6 +4,12 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
     let rect1 = Rectangle {
         width: 30,
@@ -12,7 +18,7 @@ fn main() {
 
     println!(
         "The area of the rectangle is {} sqare pixels.",
-        area(&rect1)
+        rect1.area()
     );
 
     println!("rect1:{:?}", rect1)
