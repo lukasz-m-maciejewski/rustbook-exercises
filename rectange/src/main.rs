ef44c99fe75a74d6fd1ccdaae18f1b92399873a3
Lukasz Maciejewski <lukasz.m.maciejewski@pm.me>
2020-02-26 00:26:52 +0100

    a few more examples

--- rectange/src/main.rs
+++ rectange/src/main.rs
@@ -8,20 +8,43 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
 
+use std::io::Bytes;
+
 fn main() {
+    let v : Vec<_> = vec![1, 2, 3];
     let rect1 = Rectangle {
         width: 30,
         height: 50,
     };
 
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
     println!(
-        "The area of the rectangle is {} sqare pixels.",
+        "The area of the rectangle is {} square pixels (as computed by standalone area).",
+        area(&rect1)
+    );
+
+    println!(
+        "The area of the rectangle is {} sqare pixels (as computed by method area).",
         rect1.area()
     );
 
-    println!("rect1:{:?}", rect1)
+    println!("rect1:{:?}", rect1);
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
 
 fn area(rectangle: &Rectangle) -> u32 {
