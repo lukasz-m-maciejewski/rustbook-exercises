792e5e73c8574042fd9fc5809f6bfd30acd508eb
Lukasz Maciejewski <lukasz.m.maciejewski@pm.me>
2020-03-02 23:07:44 +0100

    adding testdata; refactoring config in main.rs

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,11 +1,28 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args);
 
     println!("Searching for {}", query);
-    println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename).expect("Something went wrong when reading file!");
+
+    println!("In text:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Config { query, filename }
+    }
 }
