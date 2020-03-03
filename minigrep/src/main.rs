61120c3f0f4ed6718bc1d3518afca133779d3f0f
Lukasz Maciejewski <lukasz.m.maciejewski@pm.me>
2020-03-03 21:59:17 +0100

    extracting logic to a run function

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,17 +1,24 @@
 use std::env;
 use std::fs;
+use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let contents =
-        fs::read_to_string(config.filename).expect("Something went wrong when reading file!");
+    if let Err(e) =  run(config) {
+        println!("Application error: {}", e);
 
-    println!("In text:\n{}", contents);
+        process::exit(1);
+    }
 }
 
 struct Config {
@@ -20,10 +27,21 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments")
+        }
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contens = fs::read_to_string(config.filename)?;
+
+    println!("With text:\n{}", contens);
+
+    Ok(())
 }
